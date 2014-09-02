064f860c13de4ec12bda158a59314a8ea5b0dd4f
Thorsten Heymann <info@metashock.net>
2014-09-02 18:11:29 +0200

    Cleaned up

--- knife.rb
+++
@@ -1,7 +0,0 @@
-# Common settings
-cookbook_path ['chef/cookbooks', 'chef/site-cookbooks']
-data_bag_path "chef/data_bags"
-environment_path "chef/environments"
-node_path "chef/nodes"
-role_path "chef/roles"
-
